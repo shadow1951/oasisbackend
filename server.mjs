06e2d2927325ba91ec3c0d3ed79b5b95b1111215
Roldan <roldannov@gmail.com>
2024-12-27 09:49:52 +0530

    set-up

---
+++ server.mjs
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import { fileURLToPath } from "url";
+import path from "path";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import indexRouter from "./routes/index.mjs";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  const envPath = path.join(__dirname, ".env");
+  dotenv.config({ path: envPath });
+  mongoose.connect(process.env.DATABASE_URL);
+  console.log("Succesfully connected to the database");
+}
+const corsoption = {
+  origin: `${process.env.fDomain}`,
+  methods: "GET,HEAD,PUT,POST,DELETE",
+  credentials: true,
+};
+
+app.use(cookieParser());
+app.use(cors(corsoption));
+app.use("/", indexRouter);
+
+app.listen(
+  process.env.PORT,
+  console.log(`listening on port ${process.env.PORT}`)
+);
