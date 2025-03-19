71fb5e56aae02339b5733964cac1b623552e91c7
Roldan <roldannov@gmail.com>
2025-03-19 20:11:03 +0530

    initial commit

---
+++ model/hall.mjs
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+
+const hallModel = Schema({
+  hall_name: { type: String },
+  hall_facility: { type: String },
+  capacity: { type: Number },
+  type: { type: String },
+  primary_in_charge: { type: String },
+});
+
+export const Hall = mongoose.model("hall", hallModel);
