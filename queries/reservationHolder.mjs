d90b87a4cfdacd5b4959b40d6ecdc41d9834c34c
Roldan <roldannov@gmail.com>
2025-05-06 22:00:24 +0530

    major fix in reservation query

--- queries/reservationHolder.mjs
+++ queries/reservationHolder.mjs
@@ -1,15 +1,15 @@
 import reservationHolder from "../model/reservationHolder.mjs";
-
+import reservation from "../model/reservation.mjs";
 export const addReservationHolderQuery = async (reservationHolderData) => {
-  const { reserverName, reserverPhoneNumber, reservationEmail, id } =
+  const { reserver_name, reserver_phoneNumber, reserver_email, user_id } =
     reservationHolderData;
 
   try {
     const newReservationHolder = new reservationHolder({
-      reserverName,
-      reserverPhoneNumber,
-      reservationEmail,
-      id,
+      reserver_name,
+      reserver_phoneNumber,
+      reserver_email,
+      user_id,
     });
     const result = await newReservationHolder.save();
     return { message: "Reservation holder added successfully", result: result };
@@ -21,29 +21,46 @@ export const addReservationHolderQuery = async (reservationHolderData) => {
 
 export const addReservation = async (reservationData) => {
   const {
-    hallId,
-    eventName,
-    description,
-    startDate,
-    startTime,
-    endDate,
-    endTime,
+    reserver_id,
+    hall_id,
+    start_date,
+    end_date,
+    start_time,
+    end_time,
+    purpose,
+    status,
+    food_requirement,
   } = reservationData;
 
   try {
-    const newReservation = new reservationHolder({
-      hallId,
-      eventName,
-      description,
-      startDate,
-      startTime,
-      endDate,
-      endTime,
+    const newReservation = new reservation({
+      reserver_id,
+      hall_id,
+      start_date,
+      end_date,
+      start_time,
+      end_time,
+      purpose,
+      status,
+      food_requirement,
     });
     const result = await newReservation.save();
     return { message: "Reservation added successfully", result: result };
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error adding reservation" });
+  }
+};
+
+export const deleteReservationById = async (id) => {
+  try {
+    const result = await reservationHolder.findByIdAndDelete(id);
+    if (!result) {
+      return { message: "Reservation not found" };
+    }
+    return { message: "Reservation deleted successfully", result: result };
+  } catch (error) {
+    console.error(error);
+    return { message: "Error deleting reservation" };
   }
 };
