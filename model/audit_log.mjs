a831a1e17a3211be8e70568d6acc0d14ffb59fe3
Roldan <roldannov@gmail.com>
2025-05-07 21:38:17 +0530

    schema tweaks

--- model/audit_log.mjs
+++ model/audit_log.mjs
@@ -4,7 +4,7 @@ const auditLogSchema = new mongoose.Schema({
   reservation_id: { type: mongoose.Schema.Types.ObjectId, ref: "Reservation" },
   action: { type: String, maxlength: 50 },
   performed_by: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  timestamp: { type: Date, default: Date.now },
+  timestamp: { type: String, required: true },
 });
 
 export default mongoose.model("AuditLog", auditLogSchema);
